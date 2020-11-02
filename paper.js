945c9db11a333265fc7a790e985a2400ed3c176e
neelesh-code <71931440+neelesh-code@users.noreply.github.com>
2020-11-01 21:44:58 -0500

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,7 +1,7 @@
-class paper{
+class Paper{
     constructor(x,y,radius){
-        this.body= Matter.Bodies.circle(x,y,radius, {isStatic:false}, {restitution:0.3}, {friction:0.5}, {density:1.2});
-        this.body.radius=radius;
+        this.body= Matter.Bodies.circle(x,y,radius, {isStatic:false, restitution:0.3, friction:0.5, density:1.2});
+        this.radius=radius;
         World.add(world, this.body)
     }
 
